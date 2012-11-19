4400586b1133d4560c1ab77cc08d282153ad7f33
Андрей Терентьев <dolby0@gmail.com>
2012-11-19 17:49:01 +0400

    simple set method added

--- backbone-dotattr.js
+++ backbone-dotattr.js
@@ -1,4 +1,5 @@
 (function(_, Backbone, undefined) {
+    var oldSet = Backbone.Model.prototype.set;
     _.extend(Backbone.Model.prototype, {
         get: function(key) {
             try {
@@ -11,6 +12,32 @@
             } catch (e) {
                 return undefined;
             }
+        },
+        set: function(key, value, options){
+            if(_.isString(key)){
+                if(key.split(".").length > 1){
+                    key = key.split(".");
+                    setProperty = _.last(key);
+                    po = this;
+                    key.splice(-1, 1);
+                    _.each(key, function(key, index){
+                        po = po.get(key);
+                    });
+                    if(po instanceof Backbone.Model){
+                        po.on("change", function(){
+                            var args = ["change:" + key.join(":") +":"+ setProperty].concat(_.toArray(arguments));
+                            this.trigger.apply(this, args);
+                        }, this)
+                        return po.set(setProperty, value, options);    
+                    }else{
+                        return po[setProperty] = value;
+                    }
+                    
+                }else{
+                    return oldSet.apply(this, arguments);
+                }
+            }
+            return oldSet.apply(this, arguments);
         }
     });
 })(window._, window.Backbone);
--- test/tests.js
+++ test/tests.js
@@ -4,7 +4,10 @@ var song = new Backbone.Model({
         title: "Sgt. Pepper's Lonely Hearts Club Band",
         release: {
             year: "1987"
-        }
+        },
+        band: new Backbone.Model({
+            name: "Beatles"
+        })
     })
 });
 
@@ -35,3 +38,56 @@ test("get non-existent model attribute", function() {
     strictEqual(song.get('notpresent.title'), undefined);
     strictEqual(song.get('notpresent.release.year'), undefined);
 });
+
+test("set simple value", function(){
+    ok(song.set("title", "title"));
+    equal(song.get("title"), "title");
+    ok(song.set("title", "Lucy In The Sky With Diamonds"));
+});
+
+test("set simple nested value", function(){
+    ok(song.set("album.title", "Experience"));
+    equal(song.get("album.title"), "Experience");
+    ok(song.set("album.title", "Lucy In The Sky With Diamonds"));
+});
+
+test("set simple non-existent value", function(){
+    ok(song.set("foo", "bar"));
+    equal(song.get("foo"), "bar");
+})
+
+test("set deep nested value (nested object)", 3, function(){
+    ok(song.set("album.release.year", "2011"));
+    equal(song.get("album.release.year"), "2011");
+    ok(song.set("album.release.year", "1987"));
+});
+
+test("set deep nested value (nested model)", function(){
+    song.set("album.band.name", "ABBA");
+    equal(song.get("album.band.name"), "ABBA");
+    song.set("album.band.name", "Beatles");
+});
+
+test("change event on simple set", function(){
+    song.on("change:title", function(model, newValue, attributes){
+        equal(model, song);
+        equal(newValue, "test");
+        ok(attributes.changes.title);
+    });
+    song.set("title", "test");
+    song.off();
+    song.set("title", "Lucy In The Sky With Diamonds");
+});
+
+test("change event on simple nested set", function(){
+    // TODO: newValue must be added
+    song.on("change:album:title", function(model, attributes){
+        equal(model, song.get("album"));
+        ok(attributes.changes.title);
+    });
+    song.set("album.title", "Experience");
+    song.off();
+    song.set("album.title", "Sgt. Pepper's Lonely Hearts Club Band");
+    ok(true);
+});
+
